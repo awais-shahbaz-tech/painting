9fb13c47fe6c3571dd6fbccb19fafd23111460ba
Awais Shahbaz <awaishahbaz@gmail.com>
2024-07-18 12:51:48 +0500

    build issue resolved

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import 'regenerator-runtime/runtime';
+import "regenerator-runtime"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
