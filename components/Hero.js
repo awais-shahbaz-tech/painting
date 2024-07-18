99c3019911d9e73cef17a6fac67f076efbaf35ac
Awais Shahbaz <awaishahbaz@gmail.com>
2024-07-18 13:49:13 +0500

    chnage colr

--- components/Hero.js
+++ components/Hero.js
@@ -9,10 +9,10 @@ function Hero() {
 
   return (
     <div>
-      <Container className={"flex flex-wrap"}>
-      <div className="flex items-center w-full lg:w-1/2">
+      <Container className={"flex flex-wrap bg-[#2e0249] p-6 sm:p-8 lg:p-12 rounded-lg"}>
+      <div className="flex items-center w-full lg:w-1/2 p-6">
           <div className="max-w-2xl mb-8">
-            <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
+            <h1 className="text-4xl font-bold leading-snug tracking-tight text-white lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
             OMW, the most reliable painting service
             </h1>
             <p className="py-5 text-lg leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
@@ -30,7 +30,7 @@ function Hero() {
               <a href={`tel:${phoneNumber}`}
                
                 rel="noopener"
-                className="px-8 py-4 text-md font-medium text-center text-white bg-[#2e0249] rounded-full">
+                className="px-8 py-4 text-md font-medium text-center text-[#2e0249] bg-white rounded-full">
                 Request a Callback
               </a>
              
